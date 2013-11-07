dfefeeaa1b7f9675e8868e896d398300275ea85c
Fail Mukhametdinov <mufanu@gmail.com>
2013-11-07 20:36:24 +0600

    Create Form files

---
+++ module/Project/src/Project/Form/ProjectForm.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Admin
+ * Date: 07.11.13
+ * Time: 16:48
+ */
+
+namespace Project\Form;
+
+use Zend\Form\Form;
+use Zend\InputFilter\Factory as InputFactory;
+use Zend\InputFilter\InputFilter;
+use Project\Form\ProjectInputFilter;
+
+class ProjectForm extends Form
+{
+    public function __construct($name = null)
+    {
+        parent::__construct('project');
+        $this->setAttribute('method', 'post');
+        $this->setInputFilter(new ProjectInputFilter());
+        $this->add(array(
+            'name' => 'security',
+            'type' => 'Zend\Form\Element\Csrf',
+        ));
+        $this->add(array(
+            'name' => 'id',
+            'type' => 'Hidden',
+        ));
+        $this->add(array(
+            'name' => 'created',
+            'type' => 'Hidden',
+        ));
+        $this->add(array(
+            'name' => 'userId',
+            'type' => 'Hidden',
+        ));
+        $this->add(array(
+            'name' => 'title',
+            'type' => 'Text',
+            'options' => array(
+                'min' => 3,
+                'max' => 25,
+                'label' => 'Title',
+            ),
+        ));
+        $this->add(array(
+            'name' => 'body',
+            'type' => 'Textarea',
+            'options' => array(
+                'label' => 'Text',
+            ),
+        ));
+        $this->add(array(
+            'name' => 'state',
+            'type' => 'Checkbox',
+            'options' => array(
+                'label' => 'published',
+            ),
+        ));
+        $this->add(array(
+            'name' => 'submit',
+            'type' => 'Submit',
+            'attributes' => array(
+                'value' => 'Save',
+                'id' => 'submitbutton',
+            ),
+        ));
+    }
+}
---
+++ module/Project/src/Project/Form/ProjectInputFilter.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Admin
+ * Date: 07.11.13
+ * Time: 16:48
+ */ 
