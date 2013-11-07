010067c5a692afdcf02288dd9a007a77913298a6
Fail Mukhametdinov <mufanu@gmail.com>
2013-11-07 13:07:40 +0600

    Installed Twig

--- config/application.config.php
+++ config/application.config.php
@@ -3,13 +3,15 @@ return array(
     // This should be an array of module namespaces used in the application.
     'modules' => array(
         'Application',
+        'ZendDeveloperTools',
         'DoctrineModule',
         'DoctrineORMModule',
         'ZfcBase',
         'ZfcUser',
         'ZfcUserDoctrineORM',
         'BjyAuthorize',
-        'User'
+        'User',
+        'ZfcTwig'
     ),
 
     // These are various options for the listeners attached to the ModuleManager
