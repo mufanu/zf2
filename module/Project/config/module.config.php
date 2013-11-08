2cbd86b6b4dc38ce36a83737960f83f0ab4407a7
Fail Mukhametdinov <mufanu@gmail.com>
2013-11-08 10:46:33 +0600

    Added controller, router and view_manager

--- module/Project/config/module.config.php
+++ module/Project/config/module.config.php
@@ -12,5 +12,33 @@ return array(
                 )
             )
         )
-    )
+    ),
+    'controllers' => array(
+        'invokables' => array(
+            'Project\Controller\Project' => 'Project\Controller\ProjectController',
+        )
+    ),
+    'router' => array(
+        'routes' => array(
+            'project' => array(
+                'type' => 'segment',
+                'options' => array(
+                    'route' => '/project[/][:action][/:id]',
+                    'constraints' => array(
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id'     => '[0-9]+',
+                    ),
+                    'defaults' => array(
+                        'controller' => 'Project\Controller\Project',
+                        'action'     => 'index',
+                    ),
+                )
+            )
+        )
+    ),
+    'view_manager' => array(
+        'template_path_stack' => array(
+            __DIR__ . '/../view',
+        ),
+    ),
 );
