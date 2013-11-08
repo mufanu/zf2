e8733b3cf8ed8b0309108ef9996b5e8e851f4377
Fail Mukhametdinov <mufanu@gmail.com>
2013-11-08 10:48:03 +0600

    Write input filters

--- module/Project/src/Project/Form/ProjectInputFilter.php
+++ module/Project/src/Project/Form/ProjectInputFilter.php
@@ -4,4 +4,56 @@
  * User: Admin
  * Date: 07.11.13
  * Time: 16:48
- */ 
+ */
+
+namespace Project\Form;
+
+use Zend\Form\Form;
+use Zend\InputFilter\InputFilter;
+
+class ProjectInputFilter extends InputFilter
+{
+    public function __construct()
+    {
+        $this->add(array(
+            'name' => 'title',
+            'required' => true,
+            'validators' => array(
+                array(
+                    'name' => 'StringLength',
+                    'options' => array(
+                        'min' => 3,
+                        'max' => 100,
+                    ),
+                ),
+            ),
+            'filters' => array(
+                array('name' => 'StripTags'),
+                array('name' => 'StringTrim'),
+            ),
+
+        ));
+
+        $this->add(array(
+            'name' => 'body',
+            'required' => true,
+            'validators' => array(
+                array(
+                    'name' => 'StringLength',
+                    'options' => array(
+                        'min' => 10,
+                    ),
+                ),
+            ),
+            'filters' => array(
+                array('name' => 'StripTags'),
+                array('name' => 'StringTrim'),
+            ),
+        ));
+
+        $this->add(array(
+            'name' => 'state',
+            'required' => false,
+        ));
+    }
+}
