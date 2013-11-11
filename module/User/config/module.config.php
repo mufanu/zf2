928fd58ee78713abe1e724ede73286f5740455fe
Fail Mukhametdinov <mufanu@gmail.com>
2013-11-11 11:51:17 +0600

    Overwrite view of zfc-user

--- module/User/config/module.config.php
+++ module/User/config/module.config.php
@@ -41,4 +41,10 @@ return array(
             ),
         ),
     ),
+
+    'view_manager' => array(
+        'template_path_stack' => array(
+            __DIR__ . '/../view',
+        ),
+    ),
 );
